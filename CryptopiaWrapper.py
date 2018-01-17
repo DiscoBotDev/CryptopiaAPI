63da11f4116e8b40107317486d4c6853d037c308
DiscoBotDev <33495760+DiscoBotDev@users.noreply.github.com>
2018-01-16 16:46:36 -0800

    Initial Public Wrapper Created

---
+++ CryptopiaWrapper.py
@@ -0,0 +1,72 @@
+import requests as req
+import time
+
+#This class will handle all Cryptopia public API calls
+class Public:
+
+	#initialize Public class object
+	def __init__(self):
+		self.url = "https://www.cryptopia.co.nz/api/";
+
+	#method for getting all information from Cryptopia API for every currency. 
+	def getCurrencies(self):
+		try:
+			url = self.url + "GetCurrencies"
+			currenciesInfo = req.get(url)
+
+			return currenciesInfo.json()
+		except:
+			return f"Problems accessing {self.url}"
+
+	#method for getting information from Cryptopia API for single currency
+	def getCurrency(self, curr):
+		try:
+			url = self.url + "GetCurrencies"
+			currenciesInfo = req.get(url);
+
+			currencies = currenciesInfo.json()
+
+			currencies = currencies['Data']
+
+			currency = ''
+
+			for currinfo in currencies:
+				if currinfo['Symbol'] == curr:
+					currency = currinfo
+
+			if currency == '':
+				return "Could not find currency."
+			else:
+				return currency
+		except:
+			return f"Problems accessing {self.url}"
+
+	#method for getting information from Cryptopia API on Trade Pairs for all currencies
+	def getTradePairs(self):
+		url = self.url + "GetTradePairs";
+		TradePairsInfo = req.get(url)
+
+		return TradePairsInfo.json()
+
+	#method for getting information from Cryptopia API on an individual Trade Pair
+	def getTradePair(self, pair):
+		try:
+			url = self.url + "GetTradePairs";
+			TradePairsInfo = req.get(url);
+
+			TradePairs = TradePairsInfo.json()
+
+			TradePairs = TradePairs['Label']
+
+			TradePair = ''
+
+			for pairinfo in TradePairs:
+				if pairinfo['Symbol'] == pair:
+					TradePair = pairinfo
+
+			if TradePair == '':
+				return "Could not find currency."
+			else:
+				return TradePair
+		except:
+			return f"Problems accessing {self.url}"
